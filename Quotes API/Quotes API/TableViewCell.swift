50e63d9af869f4dae5283ef6a282eafd58ee6497
KY-2187 <kyrahung1@gmail.com>
2023-03-05 00:37:36 +0800

    Put quotes in table view

---
+++ Quotes API/Quotes API/TableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  TableViewCell.swift
+//  Quotes API
+//
+//  Created by Kyra Hung on 3/5/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    let quoteLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.alignment = .leading
+        return stack
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    func setupUI() {
+        contentView.addSubview(stackView)
+        stackView.addArrangedSubview(quoteLabel)
+
+        let constraints = [
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+        ]
+        NSLayoutConstraint.activate(constraints)    }
+}
--- Quotes API/Quotes API/ViewController.swift
+++ Quotes API/Quotes API/ViewController.swift
@@ -2,18 +2,95 @@
 //  ViewController.swift
 //  Quotes API
 //
-//  Created by Caitlin Hung on 3/4/23.
+//  Created by Kyra Hung on 3/4/23.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    private static let reuseIdentifier = "identifier"
+    var searchResponse: SearchResponse?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: ViewController.reuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        makeAPIcall{ searchResponse in
+            self.searchResponse = searchResponse
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text =  searchResponse?.results[indexPath.row].content
+        return cell
+    }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func makeAPIcall(completion: @escaping (SearchResponse?) -> Void) {
+        print("Start API Call")
+        guard let url = URL(string: "https://api.quotable.io/quotes") else {return}
+        
+        let task = URLSession.shared.dataTask(with:url) {data, response, error in
+            var searchResponse: SearchResponse?
+            defer { completion(searchResponse)}
+            print("Done with call")
+            if let error = error {
+                print("Error with API call: \(error)")
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode)
+            else {
+                print("Error with the response (\(String(describing: response))")
+                // response returned some non-successful status code
+                return
+            }
+            //if let data = data,
+               //let dataString = String(data: data, encoding: String.Encoding.utf8) {
+                //print(dataString)
+            //}
+            if let data = data,
+                let response = try? JSONDecoder().decode(SearchResponse.self, from: data) {
+                    print("Success")
+                    searchResponse = response
+                } else {
+                    print("Something is wrong with decoding, probably.")
+            }
+        }
+        task.resume()
+    }
+}
+
+struct SearchResponse:Codable {
+    let count: Int
+    let totalCount: Int
+    let page: Int
+    let totalPages: Int
+    let lastItemIndex: Int
+    let results: [Quote]
+}
+
+struct Quote:Codable {
+    let _id: String
+    let author: String
+    let content: String
 }
 
